063270f714ac6a06a7789f7f169e320a7e393a56
Sompanelle <rjray25@gmail.com>
2024-10-14 22:52:01 -0400

    visual updates

--- src/main/java/com/rj/controller/ToDoController.java
+++ src/main/java/com/rj/controller/ToDoController.java
@@ -30,7 +30,7 @@ public class ToDoController
     @GetMapping("/")
     public String Index(Model model)
     {
-        return "Index.html";
+        return "Index";
     }
 
     @GetMapping("/todoitems")
@@ -38,7 +38,7 @@ public class ToDoController
     {
         List<ToDoItemDTO> items = toDoItemService.findAllToDoItems();
         model.addAttribute("ToDoItems", items);
-        return "ToDoItems-List.html";
+        return "ToDoItems-List";
     }
 
     @GetMapping("/todoitems/search")
@@ -46,7 +46,7 @@ public class ToDoController
     {
         List<ToDoItemDTO> items = toDoItemService.SearchItemByTask(query);
         model.addAttribute("ToDoItems", items);
-        return "ToDoItems-List.html";
+        return "ToDoItems-List";
     }
 
     @GetMapping("/todoitems/new")
@@ -56,7 +56,7 @@ public class ToDoController
         List<ToDoListDTO> toDoLists = toDoListService.findAllToDoLists();
         model.addAttribute("toDoLists", toDoLists);
         model.addAttribute("toDoItem", toDoItem );
-        return "ToDoItem-New.html";
+        return "ToDoItem-New";
     }
 
     @PostMapping("/todoitems/new")
@@ -65,7 +65,7 @@ public class ToDoController
     {
         if (bindingResult.hasErrors())
         {
-            return "ToDoItem-New.html";
+            return "ToDoItem-New";
         }
         toDoItemService.SaveToDoItem(toDoItem);
         return "redirect:/todoitems";
@@ -76,6 +76,10 @@ public class ToDoController
                                Model model)
     {
         ToDoItemDTO item = toDoItemService.findItembyId(itemId);
+        if (item == null)
+        {
+            return "redirect:/todoitems";
+        }
         model.addAttribute("toDoItem", item);
         return "ToDoItem-Edit";
     }
